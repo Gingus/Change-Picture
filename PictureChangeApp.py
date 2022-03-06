c26db64254fb4528c981dacd86b33c5cc6d0e13a
PhilMinard <pminard1@outlook.com>
2022-03-06 15:54:38 +0000

    'message'

---
+++ PictureChangeApp.py
@@ -0,0 +1,64 @@
+import os
+import tkinter as tk
+import tkinter.ttk as ttk
+import pygubu
+import sys
+import os
+from PIL import Image, ImageTk
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+PROJECT_UI = os.path.join(PROJECT_PATH, "PictureChange.ui")
+
+
+class PictureChangeApp:
+    """This is to show changing a picture placed in a Label"""
+    def __init__(self, master=None):
+        self.builder = builder = pygubu.Builder()
+        builder.add_resource_path(PROJECT_PATH)
+        builder.add_from_file(PROJECT_UI)
+        self.mainwindow = builder.get_object('frame1', master)
+
+        self.labelImage = None
+        self.next_picture = None
+        self.is_on = None
+        builder.import_variables(self, ['labelImage', 'next_picture'])
+        builder.import_variables(self, ['is_on'])
+        
+        builder.connect_callbacks(self)
+    
+    def button_next_pressed(self):
+        """pressing the button to change between the images"""
+        puppy_image = 'C:\\location\\of\\Desktop\\your\\first image\\2\\to_display.jpg'
+        Phil_Pic = 'C:\\location\\of\\Desktop\\your\\first image\\2\\to_display.jpg'
+        label = self.builder.get_object('labelPicture')
+
+        if self.is_on:
+            on_image = Image.open(Phil_Pic)
+            on_image = on_image.resize((450, 350), Image.ANTIALIAS)
+            print("Resize of on_image complete")
+            image_on = ImageTk.PhotoImage(on_image)
+            label.configure(image=image_on)
+            self.is_on = False
+            print(self.is_on)
+
+        else:
+            off_image = Image.open(puppy_image)
+            off_image = off_image.resize((450, 350), Image.ANTIALIAS)
+            print("Resize of off_image complete")
+            image_off = ImageTk.PhotoImage(off_image)
+            label.configure(image=image_off)
+            self.is_on = True
+            print(self.is_on)
+        label.labelImage.update()
+        label.pack()
+
+    def run(self):
+        """Self explanatory"""
+        self.mainwindow.mainloop()
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = PictureChangeApp(root)
+    app.run()
+
---
+++ README.md
@@ -0,0 +1,27 @@
+Title:
+Python Pygubu/Tkinter on/off/Change Picture GUI
+
+Description:
+This Python GUI built with Pygubu/Tkinter is to test an on/off functionailtiy by changing between two pictures 
+that are displayed in the window.
+
+
+How to Install and Run the Project:
+Pull repo into a folder then call with python using
+
+
+How to Use the Project:
+Currently will run from IDLE/Python IDE, update will include instructions for running from the command line/Terminal.
+
+
+Include Credits:
+Pygubu - https://github.com/alejandroautalan/pygubu
+Pillow - https://pypi.org/project/Pillow/
+
+
+Add a License:
+This is intended for anyone to use and is a work in progress, whilst I'm no expert I will try to answer any question 
+about the code that 
+
+How to Contribute to the Project:
+Add Later
